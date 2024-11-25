0b43f91dfece1c915b8d48e596e00e8d4634902f
Entree3k <kmichael2090@gmail.com>
2024-11-24 17:31:36 -0800

    Delete gui directory

--- gui/styles/__init__.py
+++
@@ -1,4 +0,0 @@
-from .material import MaterialColors
-from .theme import configure_app_style
-
-__all__ = ['MaterialColors', 'configure_app_style']
--- gui/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .tooltips import create_tooltip
-from .progress_manager import ProgressManager
-
-__all__ = ['create_tooltip', 'ProgressManager']
